cc6b2caa33c20056ac7a23c6e583e1ea3daae444
Vitalii Ryzhov <vitaliiryzhov@Vitaliis-MacBook-Pro.local>
2025-04-27 12:08:15 +0200

    Cleanup: remove large files and build artifacts

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/vitaliiryzhov/Desktop/geolocation/TestGeoLocator
-# Build directory: /Users/vitaliiryzhov/Desktop/geolocation/TestGeoLocator/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("geolocator")
-subdirs("preprocessor")
-subdirs("tests")
--- build/tests/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/vitaliiryzhov/Desktop/geolocation/TestGeoLocator/tests
-# Build directory: /Users/vitaliiryzhov/Desktop/geolocation/TestGeoLocator/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(tests "/Users/vitaliiryzhov/Desktop/geolocation/TestGeoLocator/build/tests/tests")
-set_tests_properties(tests PROPERTIES  _BACKTRACE_TRIPLES "/Users/vitaliiryzhov/Desktop/geolocation/TestGeoLocator/tests/CMakeLists.txt;34;add_test;/Users/vitaliiryzhov/Desktop/geolocation/TestGeoLocator/tests/CMakeLists.txt;0;")
-subdirs("../_deps/googletest-build")
